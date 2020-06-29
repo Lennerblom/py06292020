55b7815789ee6496846ef093d471d1684488b173
Lennerblom <mlennerblom@gmail.com>
2020-06-29 18:14:33 -0400

    end of day one

---
+++ advlist/animal-list.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+
+animals = [' Fox', ' Fly',' Ant',' Bee',' Cod',' Cat',' Dog',' Yak',' Cow',' Hen',' Koi',' Hog',' Jay',' Kit']
+for animal in animals:
+    print(animal,end=" ")
---
+++ mylist01.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''doc string at the top of the code under the shebang
+'''
+
+def main():
+    movies = []    # one way to create a list
+    movies.append("Avitar")    #append = add to the end of the list
+    movies.append("My Spy")
+    print(movies[0])
+    movies.append("Ghostbusters")
+    print(movies[2])
+    print(movies[-1]
+    #print(movies.index("Ghostbusters"))
+
+
+
+if __name__ == "__main__":
+    main()
+
